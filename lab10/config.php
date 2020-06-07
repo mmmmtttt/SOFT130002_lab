54aa1126199b783e3c4f59654d3b8644aaa7eeae
mengtong <mf120511@163.com>
2020-06-07 23:50:47 +0800

    lab10

---
+++ lab10/config.php
@@ -0,0 +1,5 @@
+<?php define('DBHOST', 'localhost');  
+define('DBNAME', 'art');  
+define('DBUSER', 'mt');  
+define('DBPASS', 'Mt20001231');  
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;'); ?>
